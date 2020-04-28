8045c265dd08d51a466366009b867fe945513565
mitchsw <mitchsw@gmail.com>
2020-04-28 11:13:41 -0400

    one-handed brackets and slash reachability

--- keyboards/ergodox_ez/keymaps/mitchsw-mac/keymap.c
+++ keyboards/ergodox_ez/keymaps/mitchsw-mac/keymap.c
@@ -49,17 +49,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LCBR,                                        KC_RCBR,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_PGDOWN,
     KC_UNDS,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_COLN,
     KC_MINUS,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_LPRN,                                        KC_RPRN,        KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_QUOTE,       KC_DQUO,
-    KC_NO,          KC_NO,          KC_LALT,        KC_LCTRL,       KC_LGUI,                                                                                                        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_NO,
+    KC_LALT,        KC_LCTRL,       KC_SLASH,       KC_BSLASH,      KC_LGUI,                                                                                                        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_NO,
                                                                                                     KC_SPACE,       KC_ENTER,       KC_NO,          RSFT_W_BLOCK,
                                                                                                                     KC_DELETE,      KC_DELETE,
                                                                                     LSFT_W_BLOCK,   MO(1),          KC_BSPACE,      KC_BSPACE,      LT(1,KC_ENTER), KC_SPACE
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_NO,          KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_NO,                                          KC_NO,          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-    KC_NO,          KC__MUTE,       KC_NO,          KC_TILD,        KC_AMPR,        KC_AT,          KC_LBRACKET,                                    KC_RBRACKET,    KC_EQUAL,       KC_PLUS,        KC_NO,          KC_NO,          KC_NO,          KC_F12,
-    KC_NO,          KC__VOLDOWN,    KC__VOLUP,      KC_GRAVE,       KC_HASH,        KC_DLR,                                                                         KC_ASTR,        KC_PERC,        KC_NO,          KC_NO,          KC_NO,          WEBUSB_PAIR,         
-    KC_NO,          KC_PLAYPAU,     KC_PLAYPAU,     KC_NEXTTK,      KC_CIRC,        KC_PIPE,        KC_LABK,                                        KC_RABK,        KC_EXLM,        KC_QUES,        KC_NO,          KC_NO,          KC_NO,          KC_NO,         
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                                                                                          LCTL(KC_LEFT),  LCTL(KC_DOWN),  LCTL(KC_UP),    LCTL(KC_RIGHT), KC_NO,
+    KC_NO,          KC_NO,          KC_LCBR,        KC_RCBR,        KC_AMPR,        KC_AT,          KC_LBRACKET,                                    KC_RBRACKET,    KC_EQUAL,       KC_PLUS,        KC_TILD,        KC_NO,          KC_NO,          KC_F12,
+    KC_NO,          KC_NO,          KC_LPRN,        KC_RPRN,        KC_HASH,        KC_DLR,                                                                         KC_ASTR,        KC_PERC,        KC_GRAVE,       KC_NO,          KC_NO,          WEBUSB_PAIR,         
+    KC_NO,          KC_PLAYPAU,     KC_LABK,        KC_RABK,        KC_CIRC,        KC_PIPE,        KC_LABK,                                        KC_RABK,        KC_EXLM,        KC_QUES,        KC_NO,          KC_NO,          KC_NO,          KC_NO,         
+    KC_NO,          KC_NO,          KC_LBRACKET,    KC_RBRACKET,    KC_NO,                                                                                                          LCTL(KC_LEFT),  LCTL(KC_DOWN),  LCTL(KC_UP),    LCTL(KC_RIGHT), KC_NO,
                                                                                                     KC_NO,          KC_NO,          KC_NO,          KC_NO,         
                                                                                                                     KC_NO,          KC_NO,         
                                                                                     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,                                          KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,          KC_NO,
     KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,                                          KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,          KC_NO,
-    KC_NO,          KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,         
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,          KC_NO,          KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,         
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
@@ -108,10 +108,11 @@ uint32_t layer_state_set_user(uint32_t state) {
   ergodox_right_led_3_off();
   switch (layer) {
     case 1:
-      ergodox_right_led_3_on();
+      // Experiment with no layer leds (distracting)
+      //ergodox_right_led_3_on();
       break;
     case 2:
-      ergodox_right_led_2_on();
+      //ergodox_right_led_2_on();
       break;
     default:
       break;
