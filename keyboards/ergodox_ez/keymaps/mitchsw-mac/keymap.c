623353ac31e00c50e68a184dcba8495f3dfb3b3d
mitchsw <mitchsw@gmail.com>
2020-06-09 10:28:15 -0400

    June updates: move brackets, pgup/dn/home/end

--- keyboards/ergodox_ez/keymaps/mitchsw-mac/keymap.c
+++ keyboards/ergodox_ez/keymaps/mitchsw-mac/keymap.c
@@ -45,21 +45,21 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_NO,                                          KC_NO,          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_PGUP,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_NO,                                          KC_NO,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_PGDOWN,
+    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_NO,                                          KC_NO,          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_NO,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_NO,                                          KC_NO,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_NO,
     KC_UNDS,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_COLN,
     KC_MINUS,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_NO,                                          KC_NO,          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_QUOTE,       KC_DQUO,
-    KC_LALT,        KC_LCTRL,       KC_SLASH,       KC_BSLASH,      KC_LGUI,                                                                                                        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_NO,
+    KC_LALT,        KC_LCTRL,       LCTL(KC_LEFT),  LCTL(KC_RIGHT), KC_LGUI,                                                                                                        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_NO,
                                                                                                     KC_SPACE,       KC_ENTER,       KC_NO,          RSFT_W_BLOCK,
                                                                                                                     KC_DELETE,      KC_DELETE,
                                                                                     LSFT_W_BLOCK,   MO(1),          KC_BSPACE,      KC_BSPACE,      LT(1,KC_ENTER), KC_SPACE
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_NO,          KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_NO,                                          KC_NO,          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-    KC_NO,          KC_NO,          KC_LCBR,        KC_RCBR,        KC_AMPR,        KC_AT,          KC_NO,                                          KC_NO,          KC_EQUAL,       KC_PLUS,        KC_TILD,        KC_NO,          KC_NO,          KC_F12,
-    KC_NO,          KC_NO,          KC_LPRN,        KC_RPRN,        KC_HASH,        KC_DLR,                                                                         KC_ASTR,        KC_PERC,        KC_GRAVE,       KC_NO,          KC_NO,          WEBUSB_PAIR,         
-    KC_NO,          KC_PLAYPAU,     KC_LABK,        KC_RABK,        KC_CIRC,        KC_PIPE,        KC_NO,                                          KC_NO,          KC_EXLM,        KC_QUES,        KC_NO,          KC_NO,          KC_NO,          KC_NO,         
-    KC_NO,          KC_NO,          KC_LBRACKET,    KC_RBRACKET,    KC_NO,                                                                                                          LCTL(KC_LEFT),  LCTL(KC_DOWN),  LCTL(KC_UP),    LCTL(KC_RIGHT), KC_NO,
+    KC_NO,          KC_NO,          KC_LCBR,        KC_RCBR,        KC_AMPR,        KC_AT,          KC_NO,                                          KC_NO,          KC_EQUAL,       KC_PLUS,        KC_LABK,        KC_RABK,        KC_NO,          KC_F12,
+    KC_NO,          KC_NO,          KC_LPRN,        KC_RPRN,        KC_HASH,        KC_DLR,                                                                         KC_ASTR,        KC_PERC,        KC_LPRN,        KC_RPRN,        KC_NO,          WEBUSB_PAIR,         
+    KC_NO,          KC_PLAYPAU,     KC_SLASH,       KC_BSLASH,      KC_CIRC,        KC_PIPE,        KC_NO,                                          KC_NO,          KC_EXLM,        KC_QUES,        KC_TILD,        KC_GRAVE,       KC_NO,          KC_NO,         
+    KC_NO,          KC_NO,          LCTL(KC_DOWN),  LCTL(KC_UP),    KC_NO,                                                                                                          KC_HOME,        KC_PGDOWN,      KC_PGUP,        KC_END,         KC_NO,
                                                                                                     KC_NO,          KC_NO,          KC_NO,          KC_NO,         
                                                                                                                     KC_NO,          KC_NO,         
                                                                                     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO
