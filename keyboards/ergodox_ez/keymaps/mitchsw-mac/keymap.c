24b07996cccd106993b76bfdaf9eb953d4431b30
mitchsw <mitchsw@gmail.com>
2020-06-18 15:34:16 -0400

    revert numpad layer for now, drop lhs shift (hurts double slash with debouning)

--- keyboards/ergodox_ez/keymaps/mitchsw-mac/keymap.c
+++ keyboards/ergodox_ez/keymaps/mitchsw-mac/keymap.c
@@ -45,14 +45,14 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                                           KC_7,           KC_8,           KC_9,           KC_0,           KC_INSERT,      KC_PAUSE,       LCTL(LGUI(KC_Q)),
+    KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_NO,                                          KC_NO           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           LCTL(LGUI(KC_Q)),
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_UNDS,                                        KC_PLUS,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_PSCREEN,
     KC_BSLASH,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_COLN,
-    LSFT_T(KC_SLASH), KC_Z,         KC_X,           KC_C,           KC_V,           KC_B,           KC_MINUS,                                       KC_EQUAL,       KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_QUOTE,       KC_DQUO,
+    KC_SLASH,       KC_Z,         KC_X,           KC_C,           KC_V,           KC_B,           KC_MINUS,                                       KC_EQUAL,       KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_QUOTE,       KC_DQUO,
     KC_LALT,        KC_LCTRL,       LCTL(KC_LEFT),  LCTL(KC_RIGHT), KC_LGUI,                                                                                                        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_RCTRL,
                                                                                                     KC_SPACE,       KC_NO,          KC_NO,          RSFT_W_BLOCK,
                                                                                                                     KC_DELETE,      KC_DELETE,
-                                                                                    LSFT_W_BLOCK,   MO(1),          KC_BSPACE,      KC_BSPACE,      LT(1,KC_ENTER), LT(3,KC_SPACE)
+                                                                                    LSFT_W_BLOCK,   MO(1),          KC_BSPACE,      KC_BSPACE,      LT(1,KC_ENTER), KC_SPACE
   ),
   // Fn Layer
   [1] = LAYOUT_ergodox_pretty(
